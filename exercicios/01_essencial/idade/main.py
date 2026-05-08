d6152e448be1e9af2f236b84d3b33c09531e085b
Viviane Nascimento <vivigoncalvesporto@gmail.com>
2026-05-08 13:41:45 +0000

    Resposta ao exericio idade

--- exercicios/01_essencial/idade/main.py
+++ exercicios/01_essencial/idade/main.py
@@ -1,2 +1,7 @@
-def resposta():
-    pass
+def resposta(ano_nascimento):
+    ano_nascimento = int(ano_nascimento)
+    idade = 2026 - ano_nascimento
+    return idade
+
+ano_nascimento = input("Digite o ano: ")
+print(resposta(ano_nascimento))
